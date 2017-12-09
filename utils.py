4de03d2fda8a93a3a8d85ef179806ab69d811695
Je012 <34400058+Je012@users.noreply.github.com>
2017-12-09 11:12:17 -0500

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+
+#def onObjetClick(event):
+    #print('Got objetbt click', event.x, event.y)
+    #print(event.widget.find_closte(event.x,event.y))
+
+def coord_case(i, j, nb_pixels_per_case):
+    debut_ligne = i * nb_pixels_per_case
+    fin_ligne = debut_ligne + nb_pixels_per_case
+    debut_colonne = j * nb_pixels_per_case
+    fin_colonne = debut_colonne + nb_pixels_per_case
+    return debut_ligne, debut_colonne, fin_ligne, fin_colonne
+
+def dessiner_jeton(self, jeton, i, j, nb_pixels_per_case, tag='lettre'):
+    d = nb_pixels_per_case//2
+    debut_ligne, debut_colonne, fin_ligne, fin_colonne = coord_case(i, j, nb_pixels_per_case)
+    rectangle = self.create_rectangle(debut_colonne, debut_ligne, fin_colonne, fin_ligne, fill='#b9936c', tags=tag)
+
+    jeton = self.create_text((debut_colonne + d, debut_ligne + d),
+                     font=('Times', '{}'.format(31)), text=str(jeton), tags='lettre')
+
+    #self.tag_bind(tag, '<ButtonPress-1>', )
+    #self.tag_bind(lettre, '<ButtonPress-1>')
